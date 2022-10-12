7e19b5c8b6de85d5cacb96d2e47ddb686fc1b40b
GK8500 <qeensnkings@gmail.com>
2022-10-12 18:39:12 +0530

    yes

--- arrays/arays1.java
+++ arrays/arays1.java
@@ -5,7 +5,9 @@ public class arays1 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int[] arr = new int[size];  
+        int[] arr = new int[1000]; 
+        System.out.print("How many elements do you want in the array: "); 
+        int size = sc.nextInt()
         
         //input
         for(int i= 0; i < size ; i++){
