be6c33dfaf4c64ecb4679700ef2f5a08146e86e0
Hammad <qeensnkings@gmail.com>
2022-11-27 21:44:21 +0530

    ll

--- Collection Framework/LL.java
+++ Collection Framework/LL.java
@@ -21,10 +21,13 @@ public class LL {
             head = newNode;
             return;
         }
+// We created a new node, then made that node poit to head and then made the new node head.
         newNode.next = head;
-        head = newNode;
+        head = newNode; // head gets the new value of newNode.
 
     }
+
+    // add- Last
 
     public void addLast(String data){
         Node newNode = new Node(data);
@@ -38,6 +41,38 @@ public class LL {
         }
         currNode.next = newNode;
     }
+
+    // Delete -First
+    public void deletefirst(){
+        if(head == null){
+            System.out.println("The List is empty.");
+        }
+        head = head.next;
+    }
+    
+    // delete -last
+    
+    public void deleteLast(){
+        if(head == null){
+            System.out.println("The List is empty.");
+        }
+
+        // element next to null is also null but the code will break so we need to address this issue.
+
+        if(head.next == null){
+            head = null;
+            return;
+        }
+
+        Node secondlast = head;
+        Node lastNode = head.next;
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+            secondlast = secondlast.next;
+        }
+        secondlast.next = null;
+    }
+    // Print LL
 
     public static void printList() {
         Node currNode = head;
@@ -54,8 +89,15 @@ public class LL {
         list1.addFirst("nullashwar");
         list1.addFirst("uruguay");
         
-        list1.addLast("Aregntina");
-        
+        list1.addLast("Aregntina");        
+        list1.addLast("Zambia");        
+        list1.addLast("Congo");        
+        list1.printList();
+
+        list1.deletefirst();
+        list1.printList();
+
+        list1.deleteLast();
         list1.printList();
     }
     
