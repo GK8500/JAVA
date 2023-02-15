68af2cae6f915594ea687d62d467befc798ee142
Hammad <qeensnkings@gmail.com>
2023-02-15 19:13:41 +0530

    yes

--- sorting/selection_sort.java
+++ sorting/selection_sort.java
@@ -10,6 +10,7 @@ public class selection_sort {
 
     for(int i = 0; i < arr.length-1 ; i++) {            // get to the next element in the array
         int smallest = i;
+        
         for(int j = i + 1 ; j < arr.length; j++) {      // get to the next index in array. For eg in array [1,2,4,6,5,8,7] , the smallest index is 2nd as it was sorted in the last step so we will continue sorting from 3rd index. 
             if(arr[smallest] > arr[j]) {                // basically for replacing the smallest number if there is a change needed.
                 smallest = j;
